0cec74f16b675a881769b64ad9543c6231d856c2
zcvfcat <zcvfcat@gmail.com>
2021-12-10 15:31:14 +0900

    init

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+};
+
+export default config;
